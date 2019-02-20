7f92dce796e99c3a4106e2bedbcda03309ef5d24
Aymeric <aassier@wideagency.com>
2019-02-20 18:08:52 +0100

    Enhance intersection example page

--- src/assets/js/components/intersection.js
+++ src/assets/js/components/intersection.js
@@ -2,17 +2,12 @@ import Component from 'modulus/component'
 import viewport from 'modulus/plugins/viewport'
 
 export default class Intersection extends Component {
-
   onInit() {
 
     viewport.observe({
-      target: this.el.querySelectorAll('[data-src]'),
-      callback(el, entry) {
-        console.log('appears!', entry)
-        el.src = el.dataset.src
-      }
+      target: this.el.querySelectorAll('.outscreen'),
+      callback: el => el.classList.remove('outscreen')
     })
 
   }
-
 }
