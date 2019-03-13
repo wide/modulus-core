52572d967de9b054fec31a29bd3fa0e4f46e3933
Aymeric <aassier@wideagency.com>
2019-03-13 18:30:46 +0100

    Clean comments

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,14 @@
 import Modulus from 'modulus'
-
-// plugins
 import Viewport from 'modulus/plugins/viewport'
 import Breakpoint from 'modulus/plugins/breakpoint'
 
-// master components
 import Page from '~/masters/page'
-
-// components
 import importComponents from '[ROOT]/build/import-components'
 
-// config for plugins
 import animations from '~/utils/animations'
 import { BREAKPOINTS } from '~/consts'
 
-// modulus instance
+
 export default new Modulus({
   config: {
     debug: !process.env.PRODUCTION
