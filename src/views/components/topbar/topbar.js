b05aee4ac40a68a059b8d0c95889d4f1ae4e8628
Aymeric <aassier@wideagency.com>
2019-04-10 18:52:43 +0200

    Clean code

--- src/views/components/topbar/topbar.js
+++ src/views/components/topbar/topbar.js
@@ -7,10 +7,12 @@ export default class extends Component {
    * Initialize component 
    */
   onInit() {
+
+    // hide on scroll down, show on scroll up 
     this.$on('scroll', e => {
-      if(e.down && e.value > this.el.offsetHeight) this.el.classList.add('-hide')
-      else this.el.classList.remove('-hide')
+      this.el.classList.toggle('-hide', e.down)
     })
+    
   }
 
 }
