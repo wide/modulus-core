7dff1f6b10110daa594d020ec392682d8d5428f4
Aymeric <aassier@wideagency.com>
2019-05-20 16:01:19 +0200

    Many form fixes

--- src/assets/js/vendors/modulus/plugins/form.js
+++ src/assets/js/vendors/modulus/plugins/form.js
@@ -1,4 +1,5 @@
 import Plugin from 'modulus/plugin'
+import hotkeys from 'hotkeys-js'
 
 export default class Form extends Plugin {
 
@@ -26,38 +27,89 @@ export default class Form extends Plugin {
    * @param {HTMLElement} form 
    */
   watchForm(form) {
-    const controls = form.querySelectorAll('input, textarea, select')
-    controls.forEach(el => {
+    
+    const els = form.querySelectorAll('input, textarea, select')
 
-      // add -touched when user focus/blur
-      el.addEventListener('blur', e => {
-        el.classList.add('-touched')
-        form.classList.add('-touched')
-        this.checkValidity(form, controls)
+    // initial form check
+    this.checkFormState(form)
+    form.addEventListener('reset', e => {
+      this.setSubmitted(form, els)
+      this.checkFormState(form)
+    })
+
+    // watch children
+    els.forEach(el => {
+
+      // check validity
+      ['keyup', 'blur'].forEach(event => {
+        el.addEventListener(event, e => {
+          el.classList.add('-touched')
+          form.classList.add('-touched')
+          this.checkState(el)
+          this.checkFormState(form)
+        })
       })
 
       // add -changed when value changes
       el.addEventListener('change', e => {
         el.classList.add('-changed')
         form.classList.add('-changed')
-        this.checkValidity(form, controls)
+        this.checkState(el)
+        this.checkFormState(form)
       })
+
+      // check submit
+      hotkeys('enter', { element: el }, e => {
+        this.setSubmitted(form, els)
+      })
+
+      // initial check
+      this.checkState(el)
+    })
+
+    // watch submit
+    const btn = form.querySelector('[type=submit]')
+    if(btn) btn.addEventListener('click', e => {
+      this.setSubmitted(form, els)
     })
   }
 
 
   /**
-   * Add -invalid if cheValidity fails
-   * @param {HTMLElement} form 
-   * @param {HTMLElement} controls 
+   * Set whol form as submitted
+   * @param {HTMLELement} form 
+   * @param {NodeList} els 
    */
-  checkValidity(form, controls) {
-    for(let i = 0; i < controls.length; i++) {
-      if(!controls[i].checkValidity()) {
-        return form.classList.add('-invalid')
-      }
-    }
-    form.classList.remove('-invalid')
+  setSubmitted(form, els) {
+    form.classList.add('-submitted')
+    els.forEach(el => {
+      el.blur()
+      el.classList.add('-touched')
+    })
+  }
+
+
+  /**
+   * Check empty/invalid input state
+   * @param {HTMLElement} el 
+   */
+  checkState(el) {
+    
+    // empty value
+    el.classList[el.value ? 'remove' : 'add']('-empty')
+
+    // invalid value
+    const isInvalid = !el.checkValidity()
+    el.classList[isInvalid ? 'add' : 'remove']('-invalid')
+  }
+
+ /**
+   * Check invalid form state
+   * @param {HTMLElement} form 
+   */
+  checkFormState(form) {
+    const isInvalid = !form.checkValidity()
+    form.classList[isInvalid ? 'add' : 'remove']('-invalid')
   }
 
 }
