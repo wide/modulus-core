b7e15e0b4b7a3567c5ccdd5e604eb079dde074b9
Julien Martins Da Costa <jmartinsdacosta@wideagency.com>
2019-06-03 15:20:45 +0200

    Fix target for dataset.scrollTo

--- src/plugins/scroll/attr-scroll-to.js
+++ src/plugins/scroll/attr-scroll-to.js
@@ -4,7 +4,7 @@ export default {
     for(let i = 0; i < els.length; i++) {
       els[i].addEventListener('click', e => {
         e.stopPropagation()
-        $scroll.to(e.target.dataset.scrollto || e.target.href)
+        $scroll.to(e.target.dataset.scrollTo || e.target.href)
         return false
       })
     }
