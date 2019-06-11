29d37c03741f98c95df0c2bd2e8bc12fee649c0b
Julien Martins Da Costa <jmartinsdacosta@wideagency.com>
2019-06-11 16:20:41 +0200

    Aria-label is optional for panini icons

--- views/helpers/icon.js
+++ views/helpers/icon.js
@@ -1,3 +1,7 @@
 module.exports = (name, label = '') => {
-  return `<svg role="img" aria-label="${label}"><use xlink:href="assets/icons/sprite-icons.svg#${name}"></use></svg>`
+  if (typeof label === 'string' && label !== '') {
+    return `<svg role="img" aria-label="${label}"><use xlink:href="assets/icons/sprite-icons.svg#${name}"></use></svg>`
+  } else {
+    return `<svg role="img"><use xlink:href="assets/icons/sprite-icons.svg#${name}"></use></svg>`
+  }
 }
